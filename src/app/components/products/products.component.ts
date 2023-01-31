20b0db0893ceafc756d8d6a771e016e4b7ffaa14
jorpaz <giovanny.paz@outlook.com>
2023-01-30 23:15:31 -0600

    Services

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { Product } from '../../models/product.model'
 
+import { StoreService } from '../../services/store.service'
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -50,8 +52,14 @@ export class ProductsComponent {
     },
   ];
 
+  constructor(
+    private storeService: StoreService
+  ){
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
   onAddToShoppingCart(product: Product){
-    this.myShoppingCart.push(product);
-    this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0)
+    this.storeService.addProduct(product)
+    this.total = this.storeService.getTotal();
   }
 }
